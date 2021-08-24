f76aa086b7f8a8af5b365556aeb67df6a6463ca7
Rolando HR <rolando167@hotmail.com>
2021-08-24 14:36:13 -0500

    first commit

---
+++ Ejercicio1.js
@@ -0,0 +1,15 @@
+// Contar la cantidad de palabras que pueden existir en una cadena.
+
+
+print('hello world')
+console.log('hello world')
+
+
+
+// Helper function to print to the screen. 
+function print(line) {
+  const appDiv = document.getElementById('app');
+  const div = document.createElement('div');
+  div.innerHTML = line;
+  appDiv.appendChild(div)
+}
